462aef6262f3a9122ea20f530d0cde8be40d8afc
ifelseforeach <43578695+ifelseforeach@users.noreply.github.com>
2024-10-29 18:51:29 -0400

    Algorithms 1, Week 2, part 1

---
+++ study/Algorithms_1_Princeton,_Sedgewick/hello/DynamicConnectivityClient.java
@@ -0,0 +1,28 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.UF;
+
+public class DynamicConnectivityClient {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        int p = 0;
+        int q = 0;
+
+        UF uf = new UF(N);
+        while (!StdIn.isEmpty()) {
+            p = StdIn.readInt();
+            q = StdIn.readInt();
+            if (!uf.connected(p, q))
+                uf.union(p, q);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("0 is connected to " + i + ":" + uf.connected(0, i));
+        }
+    }
+}
